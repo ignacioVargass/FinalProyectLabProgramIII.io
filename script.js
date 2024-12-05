d3050011c6370bba03e1a99333b4c45f03f3e774
ignacioVargass <ignaciovargassosa@gmail.com>
2024-12-05 10:10:03 -0400

    script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const menuItems = [
-  { name: "Ejercicio 1", link: "ejercicios/ejercicio1/index.html" },
+  { name: "Ejercicio 1", link: "https://ignaciovargass.github.io/Etiquetas.io/" },
   { name: "Ejercicio 2", link: "ejercicios/ejercicio2/index.html" },
   // Agrega más enlaces según tus ejercicios
 ];
