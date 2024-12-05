7a8c639da965cd55a2404969f8c5c08cf2c16fe3
ignacioVargass <ignaciovargassosa@gmail.com>
2024-12-05 14:23:16 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 const menuItems = [
-  { name: "Ejercicio 1", link: "https://ignaciovargass.github.io/Etiquetas.io/" },
-  { name: "Ejercicio 2", link: "ejercicios/ejercicio2/index.html" },
+  { name: "Ejercicio 1", link: "Ejercicios/Ejercicio 1/index.html" },
+  { name: "Ejercicio 2", link: "ejercicios/ejercicio 2/index.html" },
+  { name: "Ejercicio 3", link: "ejercicios/ejercicio 3/Banreservas2/index.html" },
+  { name: "Ejercicio 3", link: "ejercicios/ejercicio 3/ValidarCedula/index.html" },
+  { name: "Ejercicio 5", link: "ejercicios/ejercicio 5/GenerarQR/index.html" },
+  { name: "Ejercicio 5", link: "ejercicios/ejercicio 5/Graficos/index.html" },
+  { name: "Ejercicio 5", link: "ejercicios/ejercicio 5/Multiplexpagina/index.html" },
+  { name: "Ejercicio 7", link: "ejercicios/ejercicio 7/index.html" },
+  { name: "Ejercicio 9", link: "ejercicios/ejercicio 9/ValidarCedula/index.html" },
+ 
   // Agrega más enlaces según tus ejercicios
 ];
 
